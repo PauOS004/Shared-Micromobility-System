89481ddd034d40ea6b54efe9079a956cb09faf3e
PauOS004 <pau.ortiz.sanchez@gmail.com>
2024-12-23 10:13:18 +0100

    Implemented GeographicPoint CLASS and TEST
    CLASS methods:
    - Constructor needs two floats (lat, lon)
    - getLatitude
    - getLongitude
    - equals
    - hashCode
    - toString
    
    TEST checks:
    - testValidGeographicPoint
    - testInvalidLatitude
    - testInvalidLongitude
    - testEqualsAndHashCode
    - testToString
    
    Co-Authored-By: GerardPueyo <189444787+gerardpueyo@users.noreply.github.com>
    Co-Authored-By: Marc Lapeña Riu <175534636+jakon-dev@users.noreply.github.com>

---
+++ src/main/java/data/GeographicPoint.java
@@ -0,0 +1,36 @@
+package data;
+
+final public class GeographicPoint {
+    private final float latitude;
+    private final float longitude;
+
+    public GeographicPoint(float lat, float lon) {
+        if (lat < -90 || lat > 90 || lon < -180 || lon > 180) {
+            throw new IllegalArgumentException("Invalid geographic coordinates");
+        }
+        this.latitude = lat;
+        this.longitude = lon;
+    }
+
+    public float getLatitude() { return latitude; }
+
+    public float getLongitude() { return longitude; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GeographicPoint gP = (GeographicPoint) o;
+        return Float.compare(gP.latitude, latitude) == 0 && Float.compare(gP.longitude, longitude) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * Float.floatToIntBits(latitude) + Float.floatToIntBits(longitude);
+    }
+
+    @Override
+    public String toString() {
+        return "GeographicPoint{latitude=" + latitude + ", longitude=" + longitude + "}";
+    }
+}
